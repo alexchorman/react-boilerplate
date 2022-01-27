d6777a66b1b67a85aa3166da98381a602b2d34c5
Oleksandr Chorman <oleksandr@ethworks.io>
2022-01-27 12:28:20 +0100

    Add instruction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,18 @@ import { TaskContainer } from '../components/tasks/TaskContainer'
 import { tasks as existingTasks } from '../data/tasks'
 import { NewForm } from '../components/forms/NewForm'
 
+// 📝 1. Create NewForm component in src/components/forms/
+// 📬 2. <NewForm/> component should accept setTasks() as a prop and update tasks array
+// 🧬 3. <NewForm/> component should include title input, description input and "Add" button which adds new tasks to the tasks array
+
 export function Home() {
   const [tasks, setTasks] = useState(existingTasks)
+
   return (
     <Page>
       <PageContent>
         <Title>TODO list</Title>
-        <NewForm onNewTask={newTask =>setTasks([newTask, ...tasks])}/>
+        <NewForm onNewTask={newTask => setTasks([newTask, ...tasks])}/>
         <TaskContainer tasks={tasks}/>
       </PageContent>
     </Page>
