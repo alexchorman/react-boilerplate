3d35ad974d2d4b27a91790fc25f0b5147cb18ff5
Oleksandr Chorman <oleksandr@ethworks.io>
2022-02-07 12:03:10 +0100

    Update instruction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,12 @@ import { Page, PageContent, Title } from '../components'
 import { TaskContainer } from '../components/tasks/TaskContainer'
 import { tasks } from '../data/tasks'
 
-// 📦 1. Create TaskContainer component inside src/components/tasks and return div with some text
-// ⬇️ 2. Import <TaskContainer/> component into Home.tsx files and check if renders correctly
-// ⬇️ 3. Import { tasks } array into Home.tsx
-// 👇 4. TaskContainer is going to accept { tasks } array as a props
-// 🎨 5. Render list of tasks. Each task should have title, checkbox, description
+// 🪝 0. Inside Home component add useState hook to track tasks changes. Use your hardcoded task array as an initial value for useState hook.
+// 📝 1. Create NewForm component in src/components/forms/.
+// 📬 2. <NewForm/> component accepts setTasks() as a prop and update tasks array.
+// 🧬 3. <NewForm/> component should include title input, description input and "Add" button.
+// 💾 4. Create internal state inside <NewForm/> component to track title and description inputs.
+// 🛠 5. On "Add" button click add new task to the tasks array using setTasks() callback.
 
 export function Home() {
   return (
