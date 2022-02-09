ffbb4f1545f195722bfa9dbbeb2cb50de2c6d44a
Oleksandr Chorman <oleksandr@ethworks.io>
2022-02-09 07:14:28 +0100

    Update instruction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,14 @@ import { TaskContainer } from '../components/tasks/TaskContainer'
 import { tasks as existingTasks } from '../data/tasks'
 import { NewForm } from '../components/forms/NewForm'
 
-// 📝 1. Create NewForm component in src/components/forms/
-// 📬 2. <NewForm/> component should accept setTasks() as a prop and update tasks array
-// 🧬 3. <NewForm/> component should include title input, description input and "Add" button which adds new tasks to the tasks array
+// 1. Create TasksProvider.tsx.
+// 2. Inside TasksProvider.tsx using createContext() create TasksContext.
+// 3. Use TasksContext.Provider to provide context for consuming components. Wrap it around <App/> component.
+// 4. Using useContext consume context state inside component you want. Use your TasksContext as an argument
+
+// 5. Create Add.tsx page and extract form code to this components.
+// 6. Inside App.tsx create new route for the Add.tsx page.
+// 7. Inside Home.tsx create link to the Add.tsx.
 
 export function Home() {
   const [tasks, setTasks] = useState(existingTasks)
