09fbae220ba0bc6dda97c1cc7c1654d1df7f2787
Oleksandr Chorman <oleksandr@ethworks.io>
2022-02-22 22:46:28 +0100

    Wrap Providers component around App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { Providers } from './providers'
 
 export function App() {
   return (
-    <Switch>
-      <Route exact path="/home" component={Home} />
-      <Redirect exact from="/" to="/home" />
-    </Switch>
+    <Providers>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Redirect exact from="/" to="/home" />
+      </Switch>
+    </Providers>
   )
 }
